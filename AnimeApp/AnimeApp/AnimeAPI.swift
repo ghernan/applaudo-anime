2181e517aabc07a815a57e39321ff265442700ef
Antonio <ghernandez@nearsoft.com>
2017-07-22 15:04:41 -0600

    BaseUrl added

---
+++ AnimeApp/AnimeApp/AnimeAPI.swift
@@ -0,0 +1,18 @@
+//
+//  AnimeAPI.swift
+//  AnimeApp
+//
+//  Created by Antonio  Hernandez  on 7/22/17.
+//  Copyright © 2017 Antonio  Hernandez . All rights reserved.
+//
+
+import Foundation
+
+
+
+struct AnimeAPI {
+    
+    static let baseURL = "https://anilist.co/api/"
+    
+    
+}
